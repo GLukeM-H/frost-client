8f3f2ce6491c84b2931cda3a67acb82f6bd21269
GLukeM-H <glukemh@gmail.com>
2021-04-13 13:49:09 -0700

    Fixed redux devtools to only be applied during development

--- src/store.js
+++ src/store.js
@@ -6,13 +6,14 @@ const initState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-	rootReducer,
-	initState,
-	compose(
-		applyMiddleware(...middleware),
+const enhancers = [applyMiddleware(...middleware)];
+
+if (process.env.NODE_ENV === "development") {
+	enhancers.push(
 		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
-);
+	);
+}
+
+const store = createStore(rootReducer, initState, compose(...enhancers));
 
 export default store;
